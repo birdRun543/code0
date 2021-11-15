adb951b3cb0bfc0c37446e2720f0448b1347ea34
birdRun543 <0804021005@stu.hznu.edu.cn>
2021-11-15 11:32:05 +0800

    update Table1Mapper.java

--- src/main/java/com/birdRun543/code0/dao/Table1Mapper.java
+++ src/main/java/com/birdRun543/code0/dao/Table1Mapper.java
@@ -4,9 +4,6 @@ import com.birdRun543.code0.model.Table1;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 
 /**
- * <p>
- * 规格类型 Mapper 接口
- * </p>
  *
  * @author hanbing
  * @since 2021-11-06
