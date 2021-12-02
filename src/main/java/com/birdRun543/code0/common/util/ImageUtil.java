9307d6dd767e7226ae8abe063b489811a6ec30fd
birdRun543 <0804021005@stu.hznu.edu.cn>
2021-12-02 20:03:59 +0800

    add ImageUtil

---
+++ src/main/java/com/birdRun543/code0/common/util/ImageUtil.java
@@ -0,0 +1,25 @@
+package com.birdRun543.code0.common.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import sun.font.FontDesignMetrics;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author hanbing
+ * @date 2021/12/2 14:29
+ */
+
+@Slf4j
+public class ImageUtil {
+
+
+}
