ebc7fbe32d77645b3ca05b2345aa4bb4f45f4dc1
birdRun543 <0804021005@stu.hznu.edu.cn>
2021-12-24 11:27:01 +0800

    update QrCodeUtil.java

--- src/main/java/com/birdRun543/code0/common/util/QrCodeUtil.java
+++ src/main/java/com/birdRun543/code0/common/util/QrCodeUtil.java
@@ -1,9 +1,38 @@
 package com.birdRun543.code0.common.util;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+
+import java.awt.image.BufferedImage;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author hanbing
  */
 public class QrCodeUtil {
 
+    public static BufferedImage getQrCode(String content)
+            throws Exception {
+        MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
 
+
+        Map<EncodeHintType, Object> hints = new HashMap<>(3);
+
+        //设置二维码四周白色区域的大小
+
+        hints.put(EncodeHintType.MARGIN, 1);
+        //设置二维码的容错性
+        hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
+        hints.put(EncodeHintType.CHARACTER_SET, "UTF-8");
+        //画二维码
+        BitMatrix bitMatrix = multiFormatWriter.encode(content, BarcodeFormat.QR_CODE, 400, 400, hints);
+        BufferedImage image = MatrixToImageWriter.toBufferedImage(bitMatrix);
+
+        return image;
+    }
 }
