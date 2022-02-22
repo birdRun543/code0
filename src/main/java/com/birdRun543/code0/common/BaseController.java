8fa4afb3965884a0e345a0734d4c9a84b95480b3
birdRun543 <0804021005@stu.hznu.edu.cn>
2022-02-22 11:27:02 +0800

    update BaseController.java

--- src/main/java/com/birdRun543/code0/common/BaseController.java
+++ src/main/java/com/birdRun543/code0/common/BaseController.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
  * @author hanbing
  */
 public class BaseController {
-    // todo 2022-02-09
+    // todo 2022-02-22
 
     @Autowired
     protected HttpServletRequest request;
